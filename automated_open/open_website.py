ceda2e3d7fced55a119703967d3c44fc0ccf4fba
A B Mitra <aditya.b.mitra@gmail.com>
2021-06-16 13:48:54 +0530

    Updated

--- automated_open/open_website.py
+++ automated_open/open_website.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 chrome_browser=webdriver.Chrome("./chromedriver")
 chrome_browser.maximize_window()
 chrome_browser.get("https://aditya22.pythonanywhere.com")
-assert 'My Website' in chrome_browser.title
+assert 'My Website' in chrome_browser
